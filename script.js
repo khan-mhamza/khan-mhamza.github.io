51cfb1440b6bf6591c64330ffe7335582bccb8e2
khan-mhamza <68407748+khan-mhamza@users.noreply.github.com>
2023-08-31 00:28:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-Let sections = document.querySelectorAll('section');
-Let navLinks = document.querySelectorAll('header nav a');
-window.onscroll = () =› {
-sections. forEach (sec =› [
-Let top = window. scrollY;
-Let offset = sec.offsetTop - 150;
-let height = sec.offsetHeight;
-Let id = sec.getAttribute('id");
-if(top >= offset && top ‹ offset + height) {
-navLinks.forEach(links =› {
-links.classList.remove ('active');
-document.querySelector('header nav a [href*=' + id + ']').classList.add ('active');
-});
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach(link => {
+        link.classList.remove('active');
+      });
+
+      document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+    }
+  });
 };
-
-});
