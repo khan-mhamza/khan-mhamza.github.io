edff851b360c4908eecf57494a66160f88545876
khan-mhamza <68407748+khan-mhamza@users.noreply.github.com>
2023-08-30 23:31:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
 const menuItems = document.querySelectorAll('.menu li a');
 menuItems.forEach(item => {
-    item.addEventListener('click', showTabContent);
+    item.addEventListener('click', scrollToSection);
 });
 
-function showTabContent(event) {
+function scrollToSection(event) {
     event.preventDefault();
-
-    // Hide all tab content sections
-    const tabContents = document.querySelectorAll('.tab-content');
-    tabContents.forEach(content => {
-        content.classList.add('hidden');
-    });
-
-    // Show the selected tab content section
     const targetId = event.target.getAttribute('href').substring(1);
     const targetSection = document.getElementById(targetId);
     if (targetSection) {
-        targetSection.classList.remove('hidden');
+        window.scrollTo({
+            top: targetSection.offsetTop,
+            behavior: 'smooth'
+        });
     }
 }
